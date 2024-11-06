cd860f308c4d29bb8e50f2dfdc7db845ba04a87c
Gustavo <gustavosales0704@gmail.com>
2024-11-06 00:39:55 -0300

    Initial commit

---
+++ src/Listas/Tarefa.java
@@ -0,0 +1,10 @@
+package Listas;
+
+public class ListaTarefas {
+    //atributo
+    private String descricao;
+
+    public ListaTarefas(String descricao) {
+        this.descricao = descricao;
+    }
+}
